e837b2ab8101e8c4cf22d07c30d70dbac1951d9e
rachad-alabi-adekambi <adekambirachad@gmail.com>
2025-01-05 19:23:59 +0100

    any sin

--- src/model/home.php
+++ src/model/home.php
@@ -6,3 +6,20 @@ function getJobs()
 
     return 'ok';
 }
+
+function newJob() {
+    $pdo = getConnexion();
+    $errors = [];
+    if (!empty($_POST)) {
+           $msg = 'done';
+           return $msg;
+        } else {
+            $response = [
+                'success' => false,
+                'errors' => $errors
+            ];
+        }
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
